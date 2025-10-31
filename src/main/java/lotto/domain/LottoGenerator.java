0cb69e255d98e6618fe0522c5b33f69f3f205354
0hj1hyeon <ojh010911@gmail.com>
2025-10-31 14:13:09 +0900

    feat: 구입한 로또 생성 로직 구현

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class LottoGenerator {
+
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    public List<Integer> generateRandomLottoNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                LOTTO_MIN_NUMBER,
+                LOTTO_MAX_NUMBER,
+                LOTTO_NUMBER_COUNT
+        );
+        Collections.sort(numbers);
+        return numbers;
+    }
+
+    public List<Lotto> generateLottos(int lottoCount) {
+        return java.util.stream.IntStream.range(0, lottoCount)
+                .mapToObj(i -> {
+                    List<Integer> randomNumbers = generateRandomLottoNumbers();
+                    return new Lotto(randomNumbers);
+                })
+                .collect(Collectors.toList());
+    }
+}
