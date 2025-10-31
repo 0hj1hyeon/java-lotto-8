357d97bc2221bc5ecad8e3d7e4f2650afcfd6fc5
0hj1hyeon <ojh010911@gmail.com>
2025-10-31 11:52:56 +0900

    feat: 구입한 금액 1000원 단위 확인 구현

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -2,7 +2,11 @@ package lotto.domain;
 
 public class PurchaseAmount {
 
+
+    private static final int LOTTO_PRICE = 1000;
+
     private static final String ERROR_NOT_POSITIVE = "[ERROR] 구입 금액은 0보다 커야 합니다.";
+    private static final String ERROR_NOT_DIVISIBLE = "[ERROR] 구입 금액은 " + LOTTO_PRICE + "원 단위로 나누어 떨어져야 합니다.";
 
     private final int amount;
 
@@ -13,11 +17,26 @@ public class PurchaseAmount {
 
     private void validate(int amount) {
         validatePositive(amount);
+        validateDivisibleByLottoPrice(amount);
     }
 
     private void validatePositive(int amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException(ERROR_NOT_POSITIVE);
         }
+    }
+
+    private void validateDivisibleByLottoPrice(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ERROR_NOT_DIVISIBLE);
+        }
+    }
+
+    public int getLottoCount() {
+        return this.amount / LOTTO_PRICE;
+    }
+
+    public int getAmount() {
+        return amount;
     }
 }
