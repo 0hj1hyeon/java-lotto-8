0642917e2500bc02f3d37d5aed0bef33ed00b85d
0hj1hyeon <ojh010911@gmail.com>
2025-10-31 23:05:57 +0900

    feat: 당첨 통계 계산 구현

---
+++ src/main/java/lotto/domain/LottoStatistic.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class LottoStatistic {
+
+    private final Map<LottoRank, Integer> statistics;
+    private final int purchaseAmount;
+
+    public LottoStatistic(BuyingLottos userLottos, WinningLotto winningLotto, PurchaseAmount purchaseAmount) {
+        this.statistics = new EnumMap<>(LottoRank.class);
+        this.purchaseAmount = purchaseAmount.getAmount();
+        initializeStatistics();
+        calculateStatistics(userLottos, winningLotto);
+    }
+
+    private void initializeStatistics() {
+        for (LottoRank rank : LottoRank.values()) {
+            statistics.put(rank, 0);
+        }
+    }
+
+    private void calculateStatistics(BuyingLottos userLottos, WinningLotto winningLotto) {
+        for (Lotto userLotto : userLottos.getBuyingLottos()) {
+
+            LottoMatchResult result = userLotto.match(winningLotto);
+            LottoRank rank = LottoRank.valueOf(result.getMatchCount(), result.isMatchBonus());
+            statistics.put(rank, statistics.get(rank) + 1);
+        }
+    }
+
+
+}
