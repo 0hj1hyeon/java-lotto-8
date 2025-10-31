b4e681dcc1cda7bfb90cc13aca7718aaba5f1fc9
0hj1hyeon <ojh010911@gmail.com>
2025-10-31 21:22:23 +0900

    feat: 당첨 번호 생성 클래스 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+
+    private static final String ERROR_BONUS_DUPLICATION = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = new Lotto(winningNumbers);
+        validateBonusDuplication(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+}
