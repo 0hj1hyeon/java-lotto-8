a7c5d2acf001b122109538489cd2aafeb94eb845
0hj1hyeon <ojh010911@gmail.com>
2025-10-31 23:08:11 +0900

    feat: 당첨 통계 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,9 @@ package lotto.view;
 
 import lotto.domain.BuyingLottos;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
@@ -18,5 +20,20 @@ public class OutputView {
                     .collect(Collectors.joining(", ", "[", "]"));
             System.out.println(numbersString);
         }
+    }
+
+    public void printStatistics(Map<LottoRank, Integer> statistics, double profitRate) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        LottoRank[] ranks = LottoRank.values();
+        for (int i = ranks.length - 2; i >= 0; i--) {
+            LottoRank rank = ranks[i];
+            if (rank != LottoRank.MISS) {
+                int count = statistics.getOrDefault(rank, 0);
+                System.out.printf("%s - %d개%n", rank.getDescription(), count);
+            }
+        }
+
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 }
