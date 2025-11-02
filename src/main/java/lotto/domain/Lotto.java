00b599c83f0534e584421c6ed5a5d5551488e17f
0hj1hyeon <ojh010911@gmail.com>
2025-11-02 12:46:43 +0900

    feat: 로또 숫자 범위 검사 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
     private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
@@ -16,6 +18,7 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
         validateDuplication(numbers);
+        validateRange(numbers);
     }
 
     // TODO: 추가 기능 구현
@@ -26,6 +29,14 @@ public class Lotto {
     private void validateDuplication(List<Integer> numbers) {
         if (new HashSet<>(numbers).size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 " + MIN_NUMBER + "부터 " + MAX_NUMBER + " 사이여야 합니다.");
+            }
         }
     }
 
